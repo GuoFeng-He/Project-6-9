10fdff02e77210acd809babc26e6105f096ffbef
BT_2N2_18 <f@f>
2024-03-15 14:05:18 -0400

    Owen - Added new rooms Guo Feng - Added lore and an enemy Jason - Working on getting Swing to work

--- Enemy.java
+++ Enemy.java
@@ -1,10 +1,38 @@
 public class Enemy {
     private int health;
     private int attack;
-    public Enemy(int health, int attack){
+    private boolean isVeteran;
+    public Enemy(int health, int attack, boolean isVeteran){
         this.health = health;
         this.attack = attack;
+        this.isVeteran = isVeteran;
     }
 
+    public int getAttack(){
+        return attack;
+    }
+    public void buffAttack(double multiplier){
+        attack *= multiplier;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public boolean getVet(){
+        return isVeteran;
+    }
 
+    // placeholder for attacks and abilities
+    public int attackOne(){
+        return 0;
+    }
+    public int attackTwo(){
+        return 0;
+    }
+    public int attackThree(){
+        return 0;
+    }
+    public void abilityOne(){}
+    public void abilityTwo(){}
+
+    public void selectAttack(){}
 }
---
+++ Wretched.java
@@ -0,0 +1,25 @@
+public class Wretched extends Enemy{
+    public Wretched(int health, int attack, boolean isVeteran){
+        super(health, attack, isVeteran);
+    }
+
+    // Attack 1: Swing
+    @Override
+    public int attackOne(){
+        if (getVet()){
+            return (int)(getAttack() * 1.5);
+        }
+        return getAttack();
+    }
+
+    @Override
+    public void abilityOne(){
+        System.out.println("The Veteran Wretched's anger grows! Its attack has increased.");
+        buffAttack(1.25);
+    }
+
+    @Override
+    public void selectAttack(){
+
+    }
+}
